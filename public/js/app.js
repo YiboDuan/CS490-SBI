67c42480e240e3fe15392ee3e5aba00389bce97c
Yibo Duan <yibo.duan@gmail.com>
2015-01-12 22:41:30 -0500

    Removed some event stuff

--- public/js/app.js
+++ public/js/app.js
@@ -4,12 +4,6 @@ var app = angular.module('dosmth', ['ngRoute', 'ngMap'])
 		$routeProvider.when('/', {
 			controller: 'smthCtrl',
 			templateUrl: 'views/mainView.html'
-		}).when('/smth/create', {
-			controller: 'smthCtrl',
-			templateUrl: 'views/smthView.html'
-		}).when('/smth/modify/:id', {
-			controller: 'smthCtrl',
-			templateUrl: 'views/smthView.html'
 		}).when('/about', {
 			controller: 'smthCtrl',
 			templateUrl: 'views/aboutView.html'
