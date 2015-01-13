7441e6cdfb3288de9bc17489cef10e5a638f4566
David Jeong <davidyjeong@gmail.com>
2015-01-12 22:24:27 -0500

    updated dir names

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/dosmth');
+mongoose.connect('mongodb://localhost/coffeerun');
 require('./models/Events');
 var routes = require('./routes/index');
 //var users = require('./routes/users');
