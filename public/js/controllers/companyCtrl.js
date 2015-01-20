2f76aa5ee28cba19b54196d7359464b1fd2df9ef
David Jeong <davidyjeong@gmail.com>
2015-01-20 15:42:40 -0500

    company search added

--- public/js/controllers/companyCtrl.js
+++ public/js/controllers/companyCtrl.js
@@ -2,10 +2,12 @@ angular.module('coffeerun')
 	.controller('companyCtrl', function companyCtrl($scope, $http, $routeParams, reviewStore) {
 		'use strict';
 
-		reviewStore.getCompany($routeParams.company).then(function(data) {
-			$scope.reviews = data.data;
-			if ($scope.reviews != null) {
-				$scope.company = {'name': $scope.reviews[0].company};
-			}
-		});
+		$scope.queryCompany = function () {
+			reviewStore.getCompany($scope.company.name).then(function(data) {
+				$scope.reviews = data.data;
+				if ($scope.reviews != null) {
+					$scope.company = {'name': $scope.reviews[0].company};
+				}
+			});
+		}
 	});
