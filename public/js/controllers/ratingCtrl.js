ec3d82ac9aad13df52f52a1c403f3b98aaa66e3b
David Jeong <davidyjeong@gmail.com>
2015-01-20 17:36:45 -0500

    edit review button updated to {{editing}}

--- public/js/controllers/ratingCtrl.js
+++ public/js/controllers/ratingCtrl.js
@@ -1,6 +1,6 @@
 angular.module('coffeerun')
   .controller('ratingCtrl', function ($scope) {
-    $scope.rate = 7;
+    $scope.rate = 0;
     $scope.max = 10;
 
     $scope.hoveringOver = function(value) {
