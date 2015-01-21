eacf4fd25a2f54742a6df56a40e47b785856e232
David Jeong <davidyjeong@gmail.com>
2015-01-20 19:08:48 -0500

    added position review

--- models/Reviews.js
+++ models/Reviews.js
@@ -3,9 +3,11 @@ var mongoose = require('mongoose');
 var ReviewSchema = new mongoose.Schema({
   company: String,
   location: String,
+  position: String,
   rating: String,
   notes: String,
-  website: String
+  website: String,
+  author: String,
 });
 
 mongoose.model('Review', ReviewSchema);
