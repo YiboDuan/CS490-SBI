5fdf07aadc9f8aca23b262fab7a1305d19332961
Yibo Duan <yibo.duan@gmail.com>
2015-01-22 18:28:09 -0500

    Merge and Add User Authentication * Session service: keep track of id and role * Authentication service: to authenticate credentials * Login Page * Login controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,18 @@
 var app = angular.module('coffeerun',['ngRoute', 'ngAutocomplete', 'ui.bootstrap'])
+	.constant('AUTH_EVENTS', {
+	  loginSuccess: 'auth-login-success',
+	  loginFailed: 'auth-login-failed',
+	  logoutSuccess: 'auth-logout-success',
+	  sessionTimeout: 'auth-session-timeout',
+	  notAuthenticated: 'auth-not-authenticated',
+	  notAuthorized: 'auth-not-authorized'
+	})
+	.constant('USER_ROLES', {
+	  all: '*',
+	  admin: 'admin',
+	  editor: 'editor',
+	  guest: 'guest'
+	})
 	.config(function ($routeProvider) {
 		'use strict';
 		$routeProvider.when('/', {
