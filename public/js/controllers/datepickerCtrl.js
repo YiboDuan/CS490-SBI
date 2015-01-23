f40cbe0dcfc2d4922522319f114526d1d1912657
David Jeong <davidyjeong@gmail.com>
2015-01-22 23:04:06 -0500

    pentagon stat indicator reviews implemented

--- public/js/controllers/datepickerCtrl.js
+++ public/js/controllers/datepickerCtrl.js
@@ -1,30 +1,31 @@
-angular.module('coffeerun').controller('datePickerCtrl', function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
+angular.module('coffeerun')
+  .controller('datePickerCtrl', function ($scope) {
+    $scope.today = function() {
+      $scope.dt = new Date();
+    };
+    $scope.today();
 
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+    $scope.clear = function () {
+      $scope.dt = null;
+    };
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
+    $scope.toggleMin = function() {
+      $scope.minDate = $scope.minDate ? null : new Date();
+    };
 
-  $scope.toggleMin();
+    $scope.toggleMin();
 
-  $scope.open = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
+    $scope.open = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
 
-    $scope.opened = true;
-  };
+      $scope.opened = true;
+    };
 
-  $scope.dateOptions = {
-    startingDay: 1
-  };
+    $scope.dateOptions = {
+      startingDay: 1
+    };
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
 });
