767e303732537fb0bca67eacce63e3fa87906ddc
David Jeong <davidyjeong@gmail.com>
2015-01-23 01:30:34 -0500

    renamed some ctrl dependancies

--- public/js/controllers/loginCtrl.js
+++ public/js/controllers/loginCtrl.js
@@ -1,9 +1,10 @@
 angular.module('coffeerun')
-  .controller('LoginController', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+  .controller('loginCtrl', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
     $scope.credentials = {
       username: '',
       password: ''
     };
+    
     $scope.login = function (credentials) {
       AuthService.login(credentials).then(function (user) {
         $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
